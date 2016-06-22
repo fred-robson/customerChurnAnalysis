94d7bb5dbf37ff9553de2ba5a3f4b648181abac3
Frederick Robson <frederickwlrobson@gmail.com>
2016-06-21 22:29:22 -0400

    Changed to class format

--- customer_churn_class.py
+++ customer_churn_class.py
@@ -1,17 +1,21 @@
 #Allows a user to analyze their customers's churn over different time periods
+__author__ = "Frederick Robson"
+__email__ = "frederickwlrobson@gmail.com"
 
+import csv;
 
-import csv
 
 class ChurnAnalysis:
 
 	def __init__(self,file_name,churn_period = 1):
 		self.churn_period = churn_period
-		self.__data = read_customer_data(file_name)
-		self.absolute = data[0]
-		self.relative = data[1]
+		self.__data = self.__read_customer_data(file_name)
+		self.absolute = self.__data[0]
+		self.relative = self.__data[1]
+		self.absolute_churn_dict = self.__calculate_churn_tp_dict(self.absolute,self.churn_period)
+		self.relative_churn_dict = self.__calculate_churn_tp_dict(self.relative,self.churn_period)
 
-	def calculate_relative(revenue_list):
+	def __calculate_relative(self, revenue_list):
 		"""
 			Takes in a list of revenues and returns the relative start date
 			@revenue_list: a list of revenue for each time period
@@ -23,11 +27,11 @@ class ChurnAnalysis:
 			count+=1
 		return revenue_list[count:]
 
-	def csv_string_to_float(revenue_list_str):
+	def __csv_string_to_float(self, revenue_list_str):
 		"""
-		Converts the csv strings to floats
-		@revenue_list_str: the list of revenue per time-period formatted as a string
-		@return: the list of revenue per time period formatted as floats 
+			Converts the csv strings to floats
+			@revenue_list_str: the list of revenue per time-period formatted as a string
+			@return: the list of revenue per time period formatted as floats 
 		"""
 		revenue_list_flt = []
 		for time_period in revenue_list_str:
@@ -39,30 +43,29 @@ class ChurnAnalysis:
 				 revenue_list_flt.append(0)
 		return revenue_list_flt
 
-	def check_churned_in_tp(customer_revenue, tp, churn_period):
+	def __check_churned_in_tp(self, customer_revenue, tp, churn_period):
 		"""
-		Checks whether a customer churned in a particular time period
-		@customer_revenue: the customer's revenue over different time periods
-		@tp: The time period for which we are checkign whether the customer has churned
-		@churn_period: How many time periods it takes a customer to churn
-		@return: True if the customer did churn in that tp, else false
+			Checks whether a customer churned in a particular time period
+			@customer_revenue: the customer's revenue over different time periods
+			@tp: The time period for which we are checkign whether the customer has churned
+			@churn_period: How many time periods it takes a customer to churn
+			@return: True if the customer did churn in that tp, else false
 		"""
 		for i in range(churn_period):
 			#Ensures that you do not try to check whether a company has churned in the future
-			#print str(tp)+"+"+str(i)+"= "+str(tp+i)
 			if(tp + i >= len(customer_revenue)): return False
 			tp_revenue = customer_revenue[tp + i]
 			if(tp_revenue > 0): return False
 		return True
 
 
-	def calculate_time_period_churned(customer_revenue, churn_period):
+	def __calculate_time_period_churned(self, customer_revenue, churn_period):
 		"""
-		Calculates the time_period in which a customer churned
-		@customer: a list containing the customers revenue for different
-		time periods
-		@churn_period: the number of time periods required to churn
-		@return: the time period churned in or null if customer did not churn
+			Calculates the time_period in which a customer churned
+			@customer: a list containing the customers revenue for different
+			time periods
+			@churn_period: the number of time periods required to churn
+			@return: the time period churned in or null if customer did not churn
 
 		"""
 		started = False #Describing whether has begun to be a customer or not
@@ -70,34 +73,31 @@ class ChurnAnalysis:
 			tp_revenue = customer_revenue[tp]
 			if(tp_revenue > 0): started = True #indicates that the customer has actually started subscribing
 			if(started and tp_revenue == 0): 
-				if(check_churned_in_tp(customer_revenue, tp, churn_period)):
+				if(self.__check_churned_in_tp(customer_revenue, tp, churn_period)):
 					return tp #return the time period the customer churned if they did churn
 
 		return None #return none if the customer didnt churn
 
-
-	def calculate_churn_rate_absolute(absolute_time_period, churn_period):
-		pass
-
-	def calculate_churn_tp_dict(relative_revenue, churn_period):
+	def __calculate_churn_tp_dict(self,relative_revenue, churn_period):
 		"""
-		Calculates the churn rate for customers in relative terms. 
-		Aka calculated the number of customers who churn one month after joining,
-		two months after joining etc
-		@relative_revenue = Revenue by customer starting at first month of subscription
-		@churn_period = the number of time periods required to churn
-		@return: a dictionary contanining the time every customer churned. If the customer did not churn, None is recorded
-		eg: {John Doe: 3} represents that customer John Doe churned in the 3rd time period after he started
+			Calculates the churn rate for customers in relative terms. 
+			Aka calculated the number of customers who churn one month after joining,
+			two months after joining etc
+			@relative_revenue = Revenue by customer starting at first month of subscription
+			@churn_period = the number of time periods required to churn
+			@return: a dictionary contanining the time every customer churned. If the customer did not churn, None is recorded
+			eg: {John Doe: 3} represents that customer John Doe churned in the 3rd time period after he started
 		"""
 		churned_time_period = {} #Dict containing the month that the customers churned
 		for customer in relative_revenue:
 			customer_revenue = relative_revenue[customer]
-			time_period_churned = calculate_time_period_churned(customer_revenue,churn_period)
+			time_period_churned = self.__calculate_time_period_churned(customer_revenue,churn_period)
 			churned_time_period[customer] = time_period_churned
 		return churned_time_period
 
 
-	def read_customer_data(filename):
+	def __read_customer_data(self, filename):
+
 		"""
 			Reads a csv file containing customer data
 			File should be formatted
@@ -111,10 +111,13 @@ class ChurnAnalysis:
 			for row in fileReader:
 				customer = row[0]
 				if(type(customer) != str): print customer
-				revenue = csv_string_to_float(row[1:])
+				revenue = self.__csv_string_to_float(row[1:])
 				absolute_time_period[customer] = revenue
-				relative_time_period[customer] = calculate_relative(revenue)
+				relative_time_period[customer] = self.__calculate_relative(revenue)
+		csvfile.close
 		return (absolute_time_period, relative_time_period)
+
+
 
 
 
